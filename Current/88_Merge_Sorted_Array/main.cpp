20e8eacb4d8b40c6a093a531b0b638d4e38cd6a3
camelboat <changxu.luo@columbia.edu>
2019-09-23 00:43:18 -0400

    Reformat code

--- Current/88_Merge_Sorted_Array/main.cpp
+++ Current/88_Merge_Sorted_Array/main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//// two pointers from the end of vectors
 class Solution {
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
@@ -12,28 +13,11 @@ public:
         int k = m + n - 1;
         while (i >= 0 && j >= 0)
         {
-            if (nums1[i] > nums2[j])
-            {
-                nums1[k] = nums1[i];
-                --i;
-            } else
-            {
-                nums1[k] = nums2[j];
-                --j;
-            }
+            if (nums1[i] > nums2[j]) {nums1[k] = nums1[i]; --i;}
+            else {nums1[k] = nums2[j]; --j;}
             --k;
         }
-        while (i >= 0)
-        {
-            nums1[k] = nums1[i];
-            --k;
-            --i;
-        }
-        while (j >= 0)
-        {
-            nums1[k] = nums2[j];
-            --k;
-            --j;
-        }
+        while (i >= 0) {nums1[k] = nums1[i]; --k; --i;}
+        while (j >= 0) {nums1[k] = nums2[j]; --k; --j;}
     }
 };
