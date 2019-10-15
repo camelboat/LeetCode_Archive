7e2ac6534ce52ca381d2f1533c0a7c3dd1363c98
camelboat <changxu.luo@columbia.edu>
2019-10-14 22:31:13 -0400

    deleted:    setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-
-/opt/clion-2019.1.4/bin/clion.sh &
-
-/opt/google/chrome/chrome --new-window https://leetcode.com/problemset/all/ &
-
-/opt/google/chrome/chrome --new-window https://leetcode.wang/ https://github.com/azl397985856/leetcode &
-
-dolphin ~/Desktop/LeetCode/current &
