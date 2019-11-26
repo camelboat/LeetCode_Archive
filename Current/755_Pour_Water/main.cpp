d3eee5c6ee1bfd9dce82d1b3b1a6f6fa3fededfc
camelboat <changxu.luo@columbia.edu>
2019-11-25 22:01:07 -0500

    new file:   Current/755_Pour_Water/main.cpp

---
+++ Current/755_Pour_Water/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> pourWater(vector<int>& heights, int V, int K) {
+        for (int i = 0; i < V; ++i) pourOneWater(heights, K);
+        return heights;
+    }
+
+    static void pourOneWater(vector<int>& heights, int K)
+    {
+        int cur = heights[K];
+        int left = INT_MAX;
+        for (int i = K-1; i >= 0; --i)
+        {
+            if (cur < heights[i]) break;
+            if (heights[i] < cur)
+            {
+                left = i;
+                cur = heights[i];
+            }
+        }
+        if (left < K)
+        {
+            heights[left]++;
+            return;
+        }
+        cur = heights[K];
+        int right = -1;
+        for (int i = K+1; i < heights.size(); ++i)
+        {
+            if (cur < heights[i]) break;
+            if (heights[i] < cur)
+            {
+                right = i;
+                cur = heights[i];
+            }
+        }
+        if (right > K)
+        {
+            heights[right]++;
+            return;
+        }
+        heights[K]++;
+    }
+};
